e992cc52a0fe5e56c123aa86364e52026f12b78c
吴思伟 <18683789594@163.com>
2022-06-15 10:52:21 +0800

    commit

--- framework-cloud-pay-api/src/main/java/com/framework/cloud/pay/api/controller/TestController.java
+++ framework-cloud-pay-api/src/main/java/com/framework/cloud/pay/api/controller/TestController.java
@@ -3,6 +3,7 @@ package com.framework.cloud.pay.api.controller;
 import com.framework.cloud.common.result.R;
 import com.framework.cloud.common.result.Result;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(path = "/test")
 public class TestController {
 
+    @ApiOperation(value = "测试")
     @GetMapping(value = "/{id}/t1")
     public Result<Long> test(@PathVariable("id") Long id) throws Exception {
         return R.success(id);
