4a0a4b760f9ee71be557757795874fdf6366ad4e
wusiwei <18683789594@163.com>
2023-01-05 14:45:57 +0800

    commit

--- framework-cloud-pay-infrastructure/src/main/java/com/framework/cloud/pay/infrastructure/mq/publish/PayPublishImpl.java
+++ framework-cloud-pay-infrastructure/src/main/java/com/framework/cloud/pay/infrastructure/mq/publish/PayPublishImpl.java
@@ -1,30 +1,30 @@
 package com.framework.cloud.pay.infrastructure.mq.publish;
 
-import com.framework.cloud.stream.properties.PayDelayProperties;
 import com.framework.cloud.pay.infrastructure.mq.PayPublish;
 import com.framework.cloud.pay.infrastructure.mq.channel.PayOrderChannel;
+import com.framework.cloud.stream.properties.StreamProperties;
 import lombok.AllArgsConstructor;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Component;
 
-import static com.framework.cloud.stream.constant.StreamConstant.TIMEOUT;
 import static com.framework.cloud.stream.constant.StreamConstant.HEADER_DELAY;
+import static com.framework.cloud.stream.constant.StreamConstant.TIMEOUT;
 /**
  * @author wusiwei
  */
 @Component
 @AllArgsConstructor
 @EnableBinding({PayOrderChannel.class})
-@EnableConfigurationProperties(PayDelayProperties.class)
+@EnableConfigurationProperties(StreamProperties.class)
 public class PayPublishImpl implements PayPublish {
 
     private final PayOrderChannel payOrderChannel;
-    private final PayDelayProperties payDelayProperties;
+    private final StreamProperties streamProperties;
 
     @Override
     public boolean publishPayOrder(String num) {
-        return payOrderChannel.output().send(MessageBuilder.withPayload(num).setHeader(HEADER_DELAY, payDelayProperties.delay(PayOrderChannel.OUT)).build(), TIMEOUT);
+        return payOrderChannel.output().send(MessageBuilder.withPayload(num).setHeader(HEADER_DELAY, streamProperties.delay(PayOrderChannel.OUT)).build(), TIMEOUT);
     }
 }
